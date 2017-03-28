3edd2f2da6c613fd8b3ecac48015f06aca658d6b
Martin Laupet <martin.laupet@outlook.com>
2017-03-28 18:25:31 +0200

    Added LoginActivity, RegisterActivity Changed startup activity in manifest

---
+++ app/src/main/java/com/hfda/LunchApp/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.hfda.LunchApp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        Log.d("Loggg", "loginActivity done");
+
+
+
+
+
+
+    // Login button Click Event
+    btnLogin.setOnClickListener(new View.OnClickListener() {
+
+        public void onClick(View view) {
+
+            //Add inputcheck!
+
+
+            //Go to next intent
+            loginClick();
+        }
+    });
+
+
+
+    }//OnCreate done
+
+
+
+
+
+    public void loginClick(){
+        Log.d("Loggg", "LoginClicked");
+        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(loginIntent);
+        //finish(); Should we use this???
+    }
+
+
+}
+
+
+
+
