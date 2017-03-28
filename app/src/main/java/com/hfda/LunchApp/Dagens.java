98fa3365cd6aafb77696a18c125cb99c5146949a
Rémi Michael Næss <ness.remi.m@gmail.com>
2017-03-28 15:55:38 +0200

    Initial commit

---
+++ app/src/main/java/com/hfda/LunchApp/Dagens.java
@@ -0,0 +1,31 @@
+package com.hfda.LunchApp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by remi_ on 28.03.2017.
+ */
+
+public class Dagens {
+    String navn;
+    Integer pris;
+    ArrayList<String> allergi = new ArrayList<>();
+
+    public Dagens(String navn, Integer pris, ArrayList<String> allergi) {
+        this.navn = navn;
+        this.pris = pris;
+        this.allergi = allergi;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public Integer getPris() {
+        return pris;
+    }
+
+    public ArrayList<String> getAllergi() {
+        return allergi;
+    }
+}
---
+++ app/src/main/java/com/hfda/LunchApp/Kontroll.java
@@ -0,0 +1,31 @@
+package com.hfda.LunchApp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by remi_ on 28.03.2017.
+ */
+
+public class Kontroll {
+
+    Bruker bruker;
+    ArrayList<Varer> varer = new ArrayList<Varer>();
+    ArrayList<Dagens> dagens = new ArrayList<Dagens>();
+
+
+    public void addNyttKaffeKort(){
+
+    }
+
+    public int getKaffekort(){
+        return 0;
+    }
+
+    public Object getDagens(){
+        return null;
+    }
+
+    public ArrayList<Varer> getVarer(){
+        return null;
+    }
+}
