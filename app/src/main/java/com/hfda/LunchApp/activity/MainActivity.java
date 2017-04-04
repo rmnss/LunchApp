1a0b13bcaac073f8ef730aae3d12bff1acafd94c
brendolini <breidet88@gmail.com>
2017-04-04 13:37:02 +0200

    Lagt til permissionsjekk for kamera i manifest og MainActivity. Lagt til QRActivity, starter QR-leseren og sjekker om riktig kode har blit lest av. Lagt til knapp som leder til QR-scanneren. Sender resultat fra QRActivity tilbake til mainActivity

--- app/src/main/java/com/hfda/LunchApp/activity/MainActivity.java
+++ app/src/main/java/com/hfda/LunchApp/activity/MainActivity.java
@@ -1,45 +1,53 @@
 package com.hfda.LunchApp.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hfda.LunchApp.helper.LunchDBhelper;
 import com.hfda.LunchApp.helper.SessionManager;
 
 import com.hfda.LunchApp.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //Må være i første acitivity
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+
+    //Må være før qr-scanneren
+    private static final int SECOND_ACTIVITY_RESULT_CODE = 0;
 
     private SessionManager session;
     private LunchDBhelper db;
     private Button btnLogout;
+
+    private String klipp = "cOFD87zuFbrYdRfKwz5m";
+    private Integer klippNR = 0;
+    private String nyttKort = "4YzY3vyBEl0gPoxuvCAB";
+    private Integer kortNr = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkRequestPermission();
+
         btnLogout = (Button) findViewById(R.id.btnLogOut);
         session = new SessionManager(getApplicationContext());
 
         //Creates database handler
         db = new LunchDBhelper(getApplicationContext());
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         // Logout button Click Event
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +58,54 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    //sjekker kameratillatelser, hvis kameraet ikke er tillat vil den spørre om det
+    private boolean checkRequestPermission() {
+        int camera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MY_PERMISSIONS_REQUEST_CAMERA);
+            return false;
+        }
+        return true;
+    }
+
+    public void onClickQR(View v) {
+        if (checkRequestPermission()) {
+            //Intent i = new Intent(getApplicationContext(), QRActivity.class);
+            //startActivity(i);
+            Intent intent = new Intent(this, QRActivity.class);
+            startActivityForResult(intent, SECOND_ACTIVITY_RESULT_CODE);
+        } else
+            Toast.makeText(this, "Godkjenn kamera-appen og trykk igjen", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SECOND_ACTIVITY_RESULT_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                String returnString = data.getStringExtra("keyName");
+
+                if (returnString.equals(klipp)) {
+                    klippNR += 1;
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(klippNR.toString());
+                } else if (returnString.equals(nyttKort)) {
+                    kortNr += 1;
+                    TextView textView2 = (TextView) findViewById(R.id.textView2);
+                    textView2.setText((kortNr.toString()));
+                } else Toast.makeText(this, "Feil kode!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 
 
---
+++ app/src/main/java/com/hfda/LunchApp/activity/QRActivity.java
@@ -0,0 +1,47 @@
+package com.hfda.LunchApp.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hfda.LunchApp.R;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.startCamera();
+    }
+
+    public void handleResult(Result rawResult) {
+
+        String stringResultat = rawResult.getText();
+        Intent intent = new Intent();
+        intent.putExtra("keyName", stringResultat);
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
