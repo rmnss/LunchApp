6ccf2c4c820b03c5335dc31efed497ec58817c32
Martin Laupet <martin.laupet@outlook.com>
2017-04-04 09:55:01 +0200

    Added register new user functionality. Takes user input and sends them to PHP. PHP adds the new user info to mySQL and then responds with true/false. User is then sent to login activity.

--- app/src/main/java/com/hfda/LunchApp/activity/RegisterActivity.java
+++ app/src/main/java/com/hfda/LunchApp/activity/RegisterActivity.java
@@ -3,12 +3,36 @@ package com.hfda.LunchApp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.hfda.LunchApp.R;
+import com.hfda.LunchApp.app.AppConfig;
+import com.hfda.LunchApp.app.AppController;
+import com.hfda.LunchApp.helper.LunchDBhelper;
+import com.hfda.LunchApp.helper.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnRegister;
+    private EditText txtName;
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private SessionManager session;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,30 +40,105 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
-        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        txtName = (EditText) findViewById(R.id.txtNewName);
+        txtEmail = (EditText) findViewById(R.id.txtNewEmail);
+        txtPassword = (EditText) findViewById(R.id.txtNewPassword);
 
-        // Login button Click Event
+
+        //creates session manager
+        session = new SessionManager(getApplicationContext());
+
+        //check if user is alerady logged in. This should not happen in this activity,
+        // but we check for safety
+        if (session.isLoggedIn()){
+            //User is logged in, send him to main activity
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+        //Register button click event
         btnRegister.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View view) {
 
-                //:TODO Add input checks
-                //:TODO Add loginfunction
+                String name = txtName.getText().toString().trim();
+                String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
 
-                //Go to next intent
-                registerClick();
+                if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+                    newUser(name, email, password);
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter all information above", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
 
 
-    public void registerClick(){
+    public void newUser(final String name, final String email, final String password){
 
-        Intent registerIntent = new Intent(RegisterActivity.this, MainActivity.class);
-        startActivity(registerIntent);
-        //finish(); should we have this?
+        // Tag used to cancel the request
+        String tag_string_req = "req_register";
+
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d("Laupet", "Register Response: " + response.toString());
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+
+                        // User successfully stored in MySQL
+                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+
+                        //Send user to loginactivity
+                        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+
+                        // Error in registration
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Laupet", "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
-
-
 }
