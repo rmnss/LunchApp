dff747355137ebfb2e2abe690ffbabe3afb8a039
Martin Laupet <martin.laupet@outlook.com>
2017-04-05 16:43:49 +0200

    App config now points to correct php files

--- app/src/main/java/com/hfda/LunchApp/app/AppConfig.java
+++ app/src/main/java/com/hfda/LunchApp/app/AppConfig.java
@@ -7,11 +7,10 @@ public class AppConfig {
     public static String URL_LOGIN = "https://home.hbv.no/884670/android/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "https://home.hbv.no/884670/android_login_api/register.php";
+    public static String URL_REGISTER = "https://home.hbv.no/884670/android/register.php";
 
     // Server user login url
     public static String URL_MENU = "https://home.hbv.no/884670/android/menu.php";
-
 
     // Server user login url
     public static String URL_DR_MENU = "https://home.hbv.no/884670/android/dr_menu.php";
