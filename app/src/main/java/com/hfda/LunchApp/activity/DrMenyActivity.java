a6bb03f96fea442506213eac2cdf22946631c338
Madeke <mads.ekeberg@gmail.com>
2017-04-06 12:15:41 +0200

    Created new activity, testing daily menu

---
+++ app/src/main/java/com/hfda/LunchApp/activity/DrMenyActivity.java
@@ -0,0 +1,22 @@
+package com.hfda.LunchApp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hfda.LunchApp.R;
+
+public class DrMenyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dr_meny);
+
+
+    }
+
+
+
+
+
+}
