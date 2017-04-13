5686cd093384cbeee7f68856836b85c1188050f5
Martin Laupet <martin.laupet@outlook.com>
2017-04-13 10:36:47 +0200

    Code cleanup + small fixes

--- app/src/main/java/com/hfda/LunchApp/fragment/HomeFragment.java
+++ app/src/main/java/com/hfda/LunchApp/fragment/HomeFragment.java
@@ -40,8 +40,7 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
