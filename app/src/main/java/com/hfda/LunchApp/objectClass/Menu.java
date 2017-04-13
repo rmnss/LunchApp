4d9010d7058a13d78a5399861135aaafc6bde21f
Martin Laupet <martin.laupet@outlook.com>
2017-04-13 14:57:20 +0200

    Recyclerview added on Menu, showing menu items. Scroll is also working

---
+++ app/src/main/java/com/hfda/LunchApp/objectClass/Menu.java
@@ -0,0 +1,33 @@
+package com.hfda.LunchApp.objectClass;
+
+public class Menu {
+
+    private String manufacturer;
+    private String name;
+    private String price; //Storing as string as we're not going to calculate with this number
+    private String category;
+
+
+    public Menu(String manufacturer, String name, String price, String category) {
+        this.manufacturer = manufacturer;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
